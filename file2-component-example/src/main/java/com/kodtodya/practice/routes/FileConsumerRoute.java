70b44f0e93331d50c0d6fa4697c74fb12230c017
Kodtodya <kodtodya@kodtodya.pnq.csb>
2019-02-04 23:10:09 +0530

    add file component

---
+++ file2-component-example/src/main/java/com/kodtodya/practice/routes/FileConsumerRoute.java
@@ -0,0 +1,18 @@
+package com.kodtodya.practice.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileConsumerRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		from("file:{{input.location}}?noop=true&initialDelay=2000&bufferSize=512").id("fileConsumer")
+		
+		.log("\n\nHi, You file content is in file consumer is>>\n\n ${body}")
+		
+		.to("direct:myDirectComponent?timeout=5000&failIfNoConsumers=false").id("directProducer");		
+	}
+
+}
---
+++ file2-component-example/src/main/java/com/kodtodya/practice/routes/FileProducerRoute.java
@@ -0,0 +1,16 @@
+package com.kodtodya.practice.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileProducerRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		from("direct:myDirectComponent").id("directConsumer")
+		.log("\n\nHi, Your file content in file producer is>>\n\n ${body}")
+		.to("file:{{output.location}}?fileName=camel-session-out.txt&fileExist=Append").id("fileProducer");		
+	}
+
+}
