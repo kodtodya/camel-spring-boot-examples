409242d635750f3da21b9950358cbea8776b7423
kodtodya <kodtodya@gmail.com>
2021-04-25 23:37:00 +0530

    new examples

---
+++ _fuse-on-openshift/http-component-example/src/main/java/com/kodtodya/practice/routes/HttpRoute.java
@@ -0,0 +1,16 @@
+package com.kodtodya.practice.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("timer:myTimer?period=2000&repeatCount=1")
+                .to("http4://www.google.com/search?q=kodtodya")
+                .log("content received from https invocation is: ${body}");
+    }
+}
