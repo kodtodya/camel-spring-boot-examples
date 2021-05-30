0c2c67cea3765e041f3dd43e692ddcb908d6054f
kodtodya <kodtodya@users.noreply.github.com>
2021-05-30 15:40:31 +0530

    Update JMSConfig.java

--- external-jms-component-example/src/main/java/com/kodtodya/practice/config/JMSConfig.java
+++ external-jms-component-example/src/main/java/com/kodtodya/practice/config/JMSConfig.java
@@ -5,11 +5,9 @@ import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
 
 import javax.jms.ConnectionFactory;
 
-@EnableJms
 @Configuration
 public class JMSConfig {
 
