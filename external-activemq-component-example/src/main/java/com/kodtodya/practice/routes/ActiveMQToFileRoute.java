4b89c4277858df7b31e328869b8476a2259f4e94
kodtodya <kodtodya@users.noreply.github.com>
2021-05-30 12:44:39 +0530

    Update ActiveMQToFileRoute.java

--- external-activemq-component-example/src/main/java/com/kodtodya/practice/routes/ActiveMQToFileRoute.java
+++ external-activemq-component-example/src/main/java/com/kodtodya/practice/routes/ActiveMQToFileRoute.java
@@ -1,6 +1,7 @@
 package com.kodtodya.practice.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
@@ -9,13 +10,17 @@ import java.util.concurrent.Executors;
 @Component
 public class ActiveMQToFileRoute extends RouteBuilder {
 
+	@Value("${spring.activemq.queue}")
+	//@PropertyInject("{{spring.activemq.queue}}")
+	private String queueName;
+
 	@Override
 	public void configure() throws Exception {
 
 		// executor service implementation for thread-pooling
 		ExecutorService executor = Executors.newFixedThreadPool(25);
 
-		from("activemq:queue:myQ").id("amq-consumer").routeId("activemq-file-route")
+		from("activemq:queue:" + queueName).id("amq-consumer").routeId("activemq-file-route")
 				// Multicast EIP allows to route the same message to a multiple endpoints & process them in a different way
 				// main difference between the Multicast and Splitter is that Splitter will split the message into several pieces
 				// but the Multicast will not modify the request message.
