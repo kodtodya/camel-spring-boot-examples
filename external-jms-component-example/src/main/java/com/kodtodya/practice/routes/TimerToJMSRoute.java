a077556b837241d1b1e2f8fb2a245e85c214ca4f
kodtodya <kodtodya@users.noreply.github.com>
2021-05-30 13:01:21 +0530

    Update TimerToJMSRoute.java

--- external-jms-component-example/src/main/java/com/kodtodya/practice/routes/TimerToJMSRoute.java
+++ external-jms-component-example/src/main/java/com/kodtodya/practice/routes/TimerToJMSRoute.java
@@ -1,17 +1,21 @@
 package com.kodtodya.practice.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TimerToJMSRoute extends RouteBuilder{
 
+	@Value("${spring.jms.queue}")
+	public String queueName;
+
 	@Override
 	public void configure() throws Exception {
-		from("timer://foo?period=1000").id("timerEndpoint").routeId("timer-jms-route")
+		from("timer://foo?period=1000").id("timer-endpoint").routeId("timer-jms-route")
 				.setBody().simple("hello buddy,  we have a problem.. and that needs to be saved in file via JMS.. :D").id("route-setBody")
 				.log("Timer endpoint triggered..").id("producer-log")
-		.to("jms:queue:myQ").id("jms-producer");
+		.to("jms:queue:" + queueName).id("jms-producer");
 	}
 
 }
