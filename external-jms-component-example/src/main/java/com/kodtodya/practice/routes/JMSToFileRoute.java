9ee751ec76fa1b80787f2d6e528a284290212023
kodtodya <kodtodya@users.noreply.github.com>
2021-05-30 13:01:02 +0530

    Update JMSToFileRoute.java

--- external-jms-component-example/src/main/java/com/kodtodya/practice/routes/JMSToFileRoute.java
+++ external-jms-component-example/src/main/java/com/kodtodya/practice/routes/JMSToFileRoute.java
@@ -1,14 +1,18 @@
 package com.kodtodya.practice.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JMSToFileRoute extends RouteBuilder{
 
+	@Value("${spring.jms.queue}")
+	public String queueName;
+
 	@Override
 	public void configure() throws Exception {
-		from("jms:queue:myQ").id("jms-consumer").routeId("jms-to-file-route")
+		from("jms:queue:" + queueName).id("jms-consumer").routeId("jms-to-file-route")
 				.log("JMS consumer triggered..").id("consumer-log")
 				.to("file:{{output.location}}").id("file-producer");
 	}
